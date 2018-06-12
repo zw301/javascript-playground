962c5b46cc7017dc83e09569e821d41fa9ae52bf
Ziyan Wang <zw301@nyu.edu>
2018-06-11 20:48:22 -0700

    Day 19 Webcam Fun

--- projects/19/scripts.js
+++ projects/19/scripts.js
@@ -4,21 +4,23 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
+
 function getVideo() {
   navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
-      console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
+      // video.src = window.HTMLMediaElement.srcObject(localMediaStream);
       video.play();
     })
     .catch(err => {
-      console.error(`OH NO!!!`, err);
+      alert("You deny the webcam.");
     });
 }
 
 function paintToCanvas() {
   const width = video.videoWidth;
   const height = video.videoHeight;
+
   canvas.width = width;
   canvas.height = height;
 
@@ -28,44 +30,47 @@ function paintToCanvas() {
     let pixels = ctx.getImageData(0, 0, width, height);
     // mess with them
     // pixels = redEffect(pixels);
-
     pixels = rgbSplit(pixels);
-    // ctx.globalAlpha = 0.8;
-
-    // pixels = greenScreen(pixels);
+    // pixels = greenScreen(pixels, 0, 0);
     // put them back
-    ctx.putImageData(pixels, 0, 0);
+    ctx.putImageData(pixels, 0, 0)
   }, 16);
 }
 
 function takePhoto() {
-  // played the sound
+  // play the sound;
   snap.currentTime = 0;
   snap.play();
 
-  // take the data out of the canvas
-  const data = canvas.toDataURL('image/jpeg');
-  const link = document.createElement('a');
+  //take the date out of the canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
   link.href = data;
-  link.setAttribute('download', 'handsome');
-  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
-  strip.insertBefore(link, strip.firsChild);
+  link.setAttribute("download", 'pretty');
+  link.innerHTML = `<img src="${data}" alt="Pretty" />`;
+  strip.insertBefore(link, strip.firstChild);
 }
 
 function redEffect(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // red
+    pixels.data[i + 0] = pixels.data[i + 0] + 200;
+    // green
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    // blue
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
   }
   return pixels;
 }
 
 function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    // red
+    pixels.data[i - 150] = pixels.data[i + 0];
+    // green
+    pixels.data[i + 100] = pixels.data[i + 1];
+    // blue
+    pixels.data[i - 150] = pixels.data[i + 2];
   }
   return pixels;
 }
@@ -98,5 +103,4 @@ function greenScreen(pixels) {
 }
 
 getVideo();
-
-video.addEventListener('canplay', paintToCanvas);
+video.addEventListener("canplay", paintToCanvas);
